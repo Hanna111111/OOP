05852ddc03962d44adf924ff625813f488fa43f7
HannaAgata <77154229+Hanna111111@users.noreply.github.com>
2022-01-16 10:07:01 +0100

    Create 1.cpp
    So we have 3 basics: encapsulation, inheritance, polymorphism.

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+//incapsulation - isolation from something, closing something foreign in order to exclude influence on the environment
+class A
+{
+ public:
+   int a, b; //open data
+   int Return_Something(); //
+ private:
+   int Aa, Ab; //hide data
+   void Do_Something(); //скрытый метод
+};
+
+//inheritance
