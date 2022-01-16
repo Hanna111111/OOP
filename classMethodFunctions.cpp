70b47cbb370862ea3c11ad3b46ac2e1a007e3954
HannaAgata <77154229+Hanna111111@users.noreply.github.com>
2022-01-16 17:33:44 +0100

    Update classMethodFunctions.cpp
    methods in classes

--- classMethodFunctions.cpp
+++ classMethodFunctions.cpp
@@ -4,8 +4,22 @@ using namespace std;
 
 //в объекте класса вместо функций - методы
 class Human{
+    
 public:
   int age;
   int weight;
   string name;
+  
+  //метод класса
+  void Print(){
+      cout << "Name: " << name << ";\nAge: " << age <<";\nWeight " << weight << ".";
+  }
+};
+
+int main(){
+    Human firstHuman;
+    firstHuman.age = 29;
+    firstHuman.weight = 100;
+    firstHuman.name = "Ivanov Ivan";
+    firstHuman.Print();
 }
